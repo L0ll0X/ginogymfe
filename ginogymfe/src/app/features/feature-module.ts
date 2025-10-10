b8e4071cafae0b72af40fe0c3592c9e88a9c25a1
Giulia Pascucci <giuliapascuccii03@gmail.com>
2025-10-10 16:37:25 +0200

    implements macchinari and detail

--- ginogymfe/src/app/features/feature-module.ts
+++ ginogymfe/src/app/features/feature-module.ts
@@ -1,27 +1,27 @@
 import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing-module';
-import { App } from './app';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { FeaturesRoutingModule } from './features-routing-module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
-  declarations: [
-    App,
-  ],
+  declarations: [],
   imports: [
-    BrowserModule,
+    CommonModule,
     RouterModule,
     HttpClientModule,
-    AppRoutingModule,
+    FeaturesRoutingModule,
+    NgbModule,
+    FormsModule
+   
   ],
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideClientHydration(withEventReplay())
-  ],
-  bootstrap: [App]
+    
+  ]
 })
-export class AppModule { }
+export class FeatureModule { }
