fecbdd12f086fb45d94878c077fcbed57a8a7b9f
Matteo Scalese <matteoscales@gmail.com>
2025-10-15 11:48:00 +0200

    add exercise

---
+++ ginogymfe/src/app/features/esercizi/esercizio-detail/esercizio-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  standalone: false,
+  templateUrl: './esercizio-detail.html',
+  styleUrl: './esercizio-detail.css'
+})
+export class Details {
+
+}
